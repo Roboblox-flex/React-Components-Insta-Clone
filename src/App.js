041f8dc0a784d3029a853eed3d8c29ae6ca7b8c4
roboblox <supeecyborg@gmail.com>
2020-07-14 14:59:37 -0400

    finished setPosts function returned SearchBar and Posts componenets in app.js return

--- src/App.js
+++ src/App.js
@@ -10,25 +10,40 @@ import React, { useState } from "react";
 import Posts from "./components/Posts/Posts.jsx";
 import SearchBar from "./components/SearchBar/SearchBar.jsx";
 // Import the dummyData
+import data from "./dummy-data";
 import "./App.css";
 
 const App = () => {
   // Create a state called 'posts' to hold the list of posts, initializing to dummyData.
+  const [posts, setPosts] = useState(data);
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
   const likePost = (postId) => {
     // This function is passed into nested components using props, to allow them to update application state.
     // It takes a post id as its only argument. The idea is to increase the 'likes' count of the post with the given `id`.
     // We will update the posts slice of state using `setPosts`, passing as the new state the invocation of `posts.map()`.
-    // The callback passed into `posts.map()` performs the following logic:
-    //  - if the `id` of the post matches `postId`, return a new post object containing an increased 'likes' count.
-    //  - otherwise just return the post object unchanged.
+
+    setPosts(
+      posts.map((post) => {
+        if (posts.id === postId) {
+          postId.likes++;
+          return post;
+        } else {
+          return post;
+        }
+
+        // The callback passed into `posts.map()` performs the following logic:
+        //  - if the `id` of the post matches `postId`, return a new post object containing an increased 'likes' count.
+        //  - otherwise just return the post object unchanged.
+      })
+    );
   };
 
   return (
     <div className="App">
       {/* Add SearchBar and Posts here to render them */}
       <SearchBar />
+      <Posts />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
